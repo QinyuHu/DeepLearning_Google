dcab3cb321b87a566c13f0a31dfbe98324c6002a
QinyuHu <qinyuhu92@gmail.com>
2016-12-17 23:12:48 -0500

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -1,5 +1,5 @@
 """Softmax."""
-
+# bigger values closer to 1, smaller values closer to 0; if scores*10, then 10 and 2 will closer to 0 than 1 and 0.2
 scores = [3.0, 1.0, 0.2]
 
 import numpy as np
